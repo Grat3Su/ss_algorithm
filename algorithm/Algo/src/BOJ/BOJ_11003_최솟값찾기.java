aea8e2fbf5c48f3ead6e937c40c0a0aab200d29e
user <>
2023-08-04 17:31:28 +0900

    algorithm

---
+++ algorithm/Algo/src/BOJ/BOJ_11003_최솟값찾기.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class BOJ_11003_최솟값찾기 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] num = new int[N];
+		
+	}
+
+}
