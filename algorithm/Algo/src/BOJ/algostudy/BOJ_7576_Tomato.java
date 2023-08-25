dd4f69b22d9599f9c14d5273c514cd4a5a383a61
user <>
2023-08-25 17:46:49 +0900

    0825

--- algorithm/Algo/src/BOJ/algostudy/BOJ_7576_Tomato.java
+++ algorithm/Algo/src/BOJ/algostudy/BOJ_7576_Tomato.java
@@ -2,6 +2,7 @@ package BOJ.algostudy;
 
 import java.util.*;
 import java.io.*;
+
 /*며칠이 지나야 토마토가 모두 익나?
  * M N
  * 6 4
@@ -11,60 +12,64 @@ import java.io.*;
 0 0 0 0 0 1
  */
 public class BOJ_7576_Tomato {
-	static int M,N, ans,day;
+	static int M, N, ans, day;
 	static int[][] box;
-			
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	public static void main(String[] args) throws Exception{
+
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	static Queue<int[]> queue = new ArrayDeque<>();
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());//상자 크기
+		N = Integer.parseInt(st.nextToken());//토마토 수
 		
-		int x = 0;
-		int y = 0;
 		box = new int[N][M];
-		for(int i = 0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<M; j++) {
+			for (int j = 0; j < M; j++) {
 				box[i][j] = Integer.parseInt(st.nextToken());
-				
-				if(box[i][j] == 1) {
-					x = i;
-					y = j;
+				if (box[i][j] == 1) {
+					queue.offer(new int[] { i,j });// 첫번쨰
 				}
 			}
 		}
-		bfs(x,y);
-		
-		for(int i = 0; i<N; i++) {
-			for(int j = 0; j<M; j++) {
-				if(box[i][j]!=1) {					
+		bfs();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				day = Math.max(day, box[i][j]);
+				if (box[i][j] == 0) {
 					System.out.println(-1);
-					//return;
-				}				
+					 return;
+				}
 			}
 		}
-		System.out.println(day);
+		System.out.println(day-1);
 	}
-	static void bfs(int x, int y) {
-		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[]{x,y});//첫번쨰
-		while(!queue.isEmpty()) {
+
+	static void bfs() {
+		while (!queue.isEmpty()) {
 			int[] cur = queue.poll();
-			day++;
-			
-			for(int i = 0; i<4; i++) {
-				int cx = cur[0]+dx[i];
-				int cy = cur[1]+dy[i];
-				
-				if(cx>-1&&cx<N&&cy>-1&&cy<M)
-					if(box[cx][cy]==0) {
-						box[cx][cy]=1;
-						queue.offer(new int[] {cx,cy});
-					}
+			int x = cur[0];
+			int y = cur[1];
+			for (int i = 0; i < 4; i++) {
+				int cx = x + dx[i];
+				int cy = y + dy[i];
+
+				if (cx < 0 || cx >= N || cy < 0 || cy >= M)
+					continue;
+
+				if (box[cx][cy] == 0) {
+					box[cx][cy] = box[x][y] + 1;
+					queue.offer(new int[] { cx, cy });
+				}
+				else if (box[cx][cy] == 1) {
+					box[cx][cy] = Math.min(box[cx][cy], box[x][y] + 1);
+				}
 			}
 		}
+
 	}
+
 }
