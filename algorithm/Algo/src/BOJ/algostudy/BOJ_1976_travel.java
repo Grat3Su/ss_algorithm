291517294c2c13805f19afdcdfcf506da337337c
Su <suyoung998@naver.com>
2023-09-13 00:43:48 +0900

    0913

--- algorithm/Algo/src/BOJ/algostudy/BOJ_1976_travel.java
+++ algorithm/Algo/src/BOJ/algostudy/BOJ_1976_travel.java
@@ -3,52 +3,76 @@ package BOJ.algostudy;
 import java.io.*;
 import java.util.*;
 
-import javax.xml.bind.ParseConversionEvent;
-
 /*N
  * M
  * adjmatrix
  * travel List
- * 3
-3
-0 1 0
-1 0 1
-0 1 0
-1 2 3
+ * 4
+2
+0 1 0 1
+1 0 1 0
+0 1 0 0
+1 0 0 0
+1 2
+-> yes
  */
 
 public class BOJ_1976_travel {
 	static int N, M;// 도시의 수/여행 도시 수
-	static int[][] adjMatrix;// 인접행렬
-	static boolean[] travelList;// 도시 리스트
+	static int[] parents;//부모
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-
-		adjMatrix = new int[N + 1][N + 1];
-
+		
+		parents = new int[N+1];
+		makeSet();
 		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {//to : i from: j
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				adjMatrix[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 1; j <= N; j++) {
+				if(Integer.parseInt(st.nextToken())==1)//이어져있으면
+					union(i,j);//결합
 			}
 		}
 		//탐색
-
-		// 방문한 도시 수
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
+		int start = find(Integer.parseInt(st.nextToken()));
+		
+		// 방문한 도시 수
+		for (int i = 1; i < M; i++) {
 			int city = Integer.parseInt(st.nextToken());
-			if (!travelList[city]) {
-				System.out.println("YES");
+			if (start!=find(city)) {//부모가 다르다
+				System.out.println("NO");
 				return;
 			}
 		}
-		System.out.println("NO");
-
+		System.out.println("YES");
+	}
+	
+	static int find(int a) {
+		if(a==parents[a])	return a;
+		
+		return parents[a] = find(parents[a]);  
+	}
+	
+	static void makeSet() {
+		for(int i = 1; i<=N; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	static void union(int a, int b){
+		int pa = find(a);//부모 찾기
+		int pb = find(b);
+		
+		if(pa==pb) return;//부모가 이미 같다 (사이클 방지)
+		
+		if(pa>pb) 
+			parents[b] = parents[a];
+		else if(pa<pb) 
+			parents[a] = parents[b];
 	}
 
 }
