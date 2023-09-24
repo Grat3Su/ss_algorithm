d6c4b045b91e6d9a69008b7780090fd9883bcc0d
Su <suyoung998@naver.com>
2023-09-25 00:19:59 +0900

    0925

--- w3/workshop_back_9/src/com/ssafy/backend/dto/User.java
+++ w3/workshop_back_9/src/com/ssafy/backend/dto/User.java
@@ -1,56 +1,57 @@
 package com.ssafy.backend.dto;
 
 public class User {
-    private String id;
-    private String name;
-    private String pass;
-    private String recId;
+	private String id;
+	private String name;
+	private String pass;
+	private String recId;
 
-    public User() {}
+	public User() {
+	}
 
-    public User(String id, String name, String pass, String recId) {
-        this.id = id;
-        this.name = name;
-        this.pass = pass;
-        this.recId = recId;
-    }
+	public User(String id, String name, String pass, String recId) {
+		this.id = id;
+		this.name = name;
+		this.pass = pass;
+		this.recId = recId;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getPass() {
-        return pass;
-    }
+	public String getPass() {
+		return pass;
+	}
 
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getRecId() {
-        return recId;
-    }
+	public String getRecId() {
+		return recId;
+	}
 
-    public void setRecId(String recId) {
-        this.recId = recId;
-    }
+	public void setRecId(String recId) {
+		this.recId = recId;
+	}
 
-    @Override
-    public String toString() {
-        return "[" + (id != null ? "id=" + id + ", " : "") + (pass != null ? "pass=" + pass + ", " : "") + (name != null ? "name=" + name + ", " : "") + (recId != null ? "recId=" + recId : "") + "]";
-    }
-
+	@Override
+	public String toString() {
+		return "[" + (id != null ? "id=" + id + ", " : "") + (pass != null ? "pass=" + pass + ", " : "")
+				+ (name != null ? "name=" + name + ", " : "") + (recId != null ? "recId=" + recId : "") + "]";
+	}
 
 }
