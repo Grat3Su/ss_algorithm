ce2b41d216df75af8a48a0a2b1a4b3b96614c266
Su <suyoung998@naver.com>
2023-10-09 23:38:33 +0900

    0

---
+++ algorithm2/algo2/src/algo_A/Bridge2.java
@@ -0,0 +1,8 @@
+package algo_A;
+
+import java.io.*;
+import java.util.*;
+
+public class Bridge2 {
+    
+}
--- algorithm2/algo2/src/algo_A/Gerrymendaring.java
+++ algorithm2/algo2/src/algo_A/Gerrymendaring.java
@@ -1,9 +1,6 @@
 package algo_A;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 /*N
  * 인구수
  * 인접 간선
@@ -17,30 +14,94 @@ import java.util.StringTokenizer;
 1 2
  */
 public class Gerrymendaring {
-	static int N;
+	static int N, ans;
 	static int[] citizen;
+	static boolean[] visit;
+	static boolean[] select;
 	static List<List<Integer>> adjlist;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		citizen = new int[N+1];
-		adjlist = new ArrayList<>();
+
+		//초기화
+		ans = Integer.MAX_VALUE;
+		citizen = new int[N];
+		select = new boolean[N];
+		visit = new boolean[N];
 		
-		for(int i = 1; i<=N; i++) {
-			adjlist.add(new ArrayList<>());
+		adjlist = new ArrayList<>();
+		for(int i = 0; i<N; i++) {
+			adjlist.add(new ArrayList<Integer>());
 		}
 		
-		for(int i = 1; i<=N; i++) {
+		for(int i = 0; i<N; i++) {
 			citizen[i] = Integer.parseInt(st.nextToken());
 		}
-		for(int i = 1; i<=N; i++) {
+		for(int i = 0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			for(int j = 0; j<n; j++)
-				adjlist.get(i).add(Integer.parseInt(st.nextToken()));
+				adjlist.get(i).add(Integer.parseInt(st.nextToken())-1);
 		}
 
+		Divide(0);
+		System.out.println(ans == Integer.MAX_VALUE? -1:ans);
+
+	}
+
+	static void Divide(int idx){
+		if(N == idx){
+			List<Integer> alist = new ArrayList<>();
+			List<Integer> blist = new ArrayList<>();
+
+			for(int i = 0; i<N; i++){
+				if(select[i]) alist.add(i);
+				else if(!select[i]) blist.add(i);				
+			}
+			if(alist.size() == 0|| blist.size() == 0) return;//한 지역에 모두 있으면 안됨
+			if(Check(alist)&&Check(blist)){
+				int asum = 0;
+				int bsum = 0;
+
+				for(int i = 0; i<N; i++){
+					if(select[i])//A구역
+					asum+=citizen[i];
+					else
+					bsum += citizen[i];
+				}
+
+				ans = Math.min(ans, Math.abs(asum-bsum));
+			}			
+			return;
+		}
+
+		select[idx] = true;
+		Divide(idx+1);
+		select[idx] = false;
+		Divide(idx+1);
+	}
+
+	static boolean Check(List<Integer> list){//구역 확인
+	Queue<Integer>q = new ArrayDeque<>();
+		visit = new boolean[N];
+		visit[list.get(0)] = true;
+		q.offer(list.get(0));
+		int cnt = 1;//방문한 지역 수
+		while(!q.isEmpty()){
+			int cur = q.poll();
+			for(int i = 0; i<adjlist.get(cur).size(); i++){
+				int n = adjlist.get(cur).get(i);
+				if(list.contains(n)&& !visit[n]){
+					q.offer(n);
+					visit[n] = true;
+					cnt++;
+				}
+			}
+		}		
+		
+		if(cnt == list.size()) return true;//선거구 수 = 방문한 지역 수
+		else return false;
 	}
 
 }
